edb968eeb053ae7d3181fdd813f99c6247ec6702
Madis-Julius <estonira@gmail.com>
2026-04-03 19:02:55 +0300

    Add project files.

---
+++ ISeeYou/Data/Extensions/IAuditExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace ISeeYou.Data.Extensions
+{
+    public static class IAuditExtensions
+    {
+        public static void SetCreated(this IAudit audit, Guid? userId = null)
+        {
+            audit.CreatedAtUtc = DateTime.UtcNow;
+            audit.ModificationBy = userId;
+        }
+        public static void SetUpdated(this IAudit audit, Guid? userId = null)
+        {
+            audit.UpdatedAtUtc = DateTime.UtcNow;
+            audit.ModificationBy = userId;
+        }
+    }
+}
---
+++ ISeeYou/Data/IAudit.cs
@@ -0,0 +1,9 @@
+﻿namespace ISeeYou.Data
+{
+    public interface IAudit
+    {
+        DateTime CreatedAtUtc { get; set; }
+        DateTime? UpdatedAtUtc { get; set; }
+        Guid? ModificationBy { get; set; }
+    }
+}
